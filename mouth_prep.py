6c6a81156f1fc7b7107f9a80f75dadd07af38ea4
Arild Matsson <arild@klavaro.se>
2017-12-22 16:15:19 +0100

    7x7x2048 resnet data, removed npy because 1.4 GB

--- mouth_prep.py
+++ mouth_prep.py
@@ -1,8 +1,10 @@
+import sys
+
 from keras.applications.imagenet_utils import preprocess_input
 from keras.applications.resnet50 import ResNet50
+from keras.engine.training import Model
 from keras.preprocessing.image import load_img, img_to_array
 import numpy as np
-import sys
 
 MOUTHING_DATA_DIR = 'phoenix-mouthing-ECCV'
 
@@ -33,24 +35,23 @@ def mouthing_data(limit=None):
 
 def resnet_convert(paths):
     print('Loading ResNet50')
-    resnet = ResNet50(False)
+    resnet = ResNet50(weights='imagenet', include_top=False)
+    # Discard last pooling & activation layer.
+    resnet = Model(resnet.inputs, resnet.layers[-2].output)
+
     print('Loading images')
     images = load_image_input(paths)
     print('Processing images through ResNet50')
-    return resnet.predict(images).reshape((len(images), 2048))
+    return resnet.predict(images)
 
 def mouthing_data_resnet(limit=None):
     paths_data = mouthing_data(limit)
     paths = (path for path, lalbel in paths_data)
-    label_map = LabelMap()
-    labels = [label_map[label] for path, label in paths_data]
 
     resnet_data = resnet_convert(paths)
-    # Add label item to each image resnet vector.
-    cat_data = np.c_[resnet_data, labels]
-    save_filename = 'mouthing-frames-resnet-labels.npy'
-    print('Saving data of shape {} to {}'.format(cat_data.shape, save_filename))
-    np.save(save_filename, cat_data)
+    save_filename = 'mouthing-frames-resnet.npy'
+    print('Saving data of shape {} to {}'.format(resnet_data.shape, save_filename))
+    np.save(save_filename, resnet_data)
 
 if __name__ == '__main__':
     mouthing_data_resnet(*sys.argv[1:])
