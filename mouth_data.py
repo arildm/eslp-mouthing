1dcbf07b72307d17942d26e4e1f70d29e8b0bf0b
Arild Matsson <arild@klavaro.se>
2017-12-25 16:49:19 +0100

    Collected data functions in mouth_data.py, completed model and training function

--- mouth_data.py
+++ mouth_data.py
@@ -1,9 +1,8 @@
-import sys
-
 from keras.applications.imagenet_utils import preprocess_input
 from keras.applications.resnet50 import ResNet50
 from keras.engine.training import Model
 from keras.preprocessing.image import load_img, img_to_array
+from keras.utils.np_utils import to_categorical
 import numpy as np
 
 MOUTHING_DATA_DIR = 'phoenix-mouthing-ECCV'
@@ -45,5 +44,35 @@ def mouthing_data_resnet(limit=None):
     print('Saving data of shape {} to {}'.format(resnet_data.shape, save_filename))
     np.save(save_filename, resnet_data)
 
-if __name__ == '__main__':
-    mouthing_data_resnet(*sys.argv[1:])
+def read_frames_data():
+    """Loads the ResNet50-converted frame images."""
+    data_filename = 'mouthing-frames-resnet.npy'
+    resnet_data = np.load(data_filename)
+    print('Loaded data of shape {} from {}'.format(resnet_data.shape, data_filename))
+    return resnet_data
+
+def load_label_map():
+    """Loads labels as 1) a map to ids and 2) one-hot arrays of ids in order."""
+    id_map = dict()
+    with open(MOUTHING_DATA_DIR + '/annotations/label-id') as map_file:
+        for line in map_file.read().splitlines():
+            id, label = line.split(' ')
+            # Their ids in the file are 1-indexed. Switch to 0-indexed by
+            # subtracting 1, so our id = index.
+            id_map[label] = int(id) - 1
+
+    cats = to_categorical(sorted(id_map.values()))
+    return id_map, cats
+
+def load_mouth_annotations():
+    """Loads annotations, converts to onehot and returns as list."""
+    label_map, label_onehots = load_label_map()
+    with open(MOUTHING_DATA_DIR + '/annotations/mouthing.annotations') as annotations_file:
+        lines = annotations_file.read().splitlines()
+        items = []
+        for line in lines:
+            path, labels_all = line.split(' ')
+            labels = labels_all.split('-')
+            onehot = sum(label_onehots[label_map[label]] for label in labels) / len(labels)
+            items.append(onehot)
+        return items
--- mouth_train.py
+++ mouth_train.py
@@ -1,48 +1,38 @@
-from keras.layers.core import Activation, Dense
+from keras.layers.core import Activation, Dense, Reshape
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
-from keras.utils.np_utils import to_categorical
+import nltk
 import numpy as np
 
-MOUTHING_DATA_DIR = 'phoenix-mouthing-ECCV'
+import mouth_data
 
-def read_frames_data():
-    """Loads the ResNet50-converted frame images."""
-    data_filename = 'mouthing-frames-resnet.npy'
-    resnet_data = np.load(data_filename)
-    print('Loaded data of shape {} from {}'.format(resnet_data.shape, data_filename))
-    return resnet_data
-
-def load_label_map():
-    """Loads labels as 1) a map to ids and 2) one-hot arrays of ids in order."""
-    id_map = dict()
-    with open(MOUTHING_DATA_DIR + '/annotations/label-id') as map_file:
-        for line in map_file.read().splitlines():
-            id, label = line.split(' ')
-            id_map[label] = int(id)
-
-    cats = to_categorical(sorted(range(max(id_map.values()) + 1)))
-    return id_map, cats
-
-def load_mouth_annotations():
-    """Loads annotations, converts to onehot and returns as list."""
-    label_map, label_onehots = load_label_map()
-    with open(MOUTHING_DATA_DIR + '/annotations/mouthing.annotations') as annotations_file:
-        lines = annotations_file.read().splitlines()
-        items = []
-        for line in lines:
-            path, labels_all = line.split(' ')
-            labels = labels_all.split('-')
-            onehot = sum(label_onehots[label_map[label]] for label in labels) / len(labels)
-            items.append(onehot)
-        return items
-
-def create_model(nlabels):
+def create_model():
     """Creates an RNN model for sequential frame data input and label output."""
     model = Sequential()
-    model.add(LSTM(64, input_shape=(2, 7, 7, 2048)))
-    model.add(Dense(nlabels))
+    # Flatten 3D to 1D but keep the bigram dimension.
+    model.add(Reshape((2, 7 * 7 * 2048), input_shape=(2, 7, 7, 2048)))
+    model.add(LSTM(32))
+    # Output layer the size of the number of labels.
+    model.add(Dense(40))
     model.add(Activation('softmax'))
-
-    model.compile('adam', 'categorical_crossentropy', ['accuracy', 'recall'])
+    model.compile('adam', 'categorical_crossentropy', ['accuracy'])
+    model.summary()
     return model
+
+def train_evaluate_model():
+    """Trains and saves a model."""
+    model = create_model()
+
+    # Load frames data and convert to bigrams.
+    frames_data = mouth_data.read_frames_data()
+    frames_data_bigrams = np.array(list(nltk.bigrams(frames_data)))
+
+    # Load annotations; skip first annotation because input is bigrams.
+    mouth_annotations = np.array(mouth_data.load_mouth_annotations()[1:])
+
+    # Train and save model.
+    model.fit(frames_data_bigrams, mouth_annotations)
+    model.save('mouthing-model.h5')
+
+if __name__ == '__main__':
+    train_evaluate_model()
